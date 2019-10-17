c89d42ca3b69d9b73e4debc139f7ae8bc5d4e642
uzaisindiko <uzai.sindiko@gmail.com>
2019-10-17 21:57:57 +0700

    tampilan

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,22 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <div>
-  <b-card
-    title="Card Title"
-    img-src="https://picsum.photos/600/300/?image=25"
-    img-alt="Image"
-    img-top
-    tag="article"
-    style="max-width: 20rem;"
-    class="mb-2"
-  >
-    <b-card-text>
-      Some quick example text to build on the card title and make up the bulk of the card's content.
-    </b-card-text>
-
-    <b-button href="#" variant="primary">Go somewhere</b-button>
-  </b-card>
-</div>
+  <div class="race d-flex flex-column justify-content-around align-items-center">
+    <road v-for="(player, index) in players" :key="index" :car="cars[index]"/>
   </div>
 </template>
+
+<script>
+import road from '@/components/road.vue'
+
+export default {
+  components: {
+    road
+  },
+
+  data(){
+    return {
+      players: ['player1', 'player2', 'player3', 'player4'],
+      cars:['https://img.icons8.com/color/48/000000/car.png', 
+      'https://img.icons8.com/color/48/000000/electric-bumper-car.png',
+      'https://img.icons8.com/dusk/64/000000/encashment-car.png',
+      'https://img.icons8.com/dusk/64/000000/car-roof-box.png']
+    }
+  }
+}
+</script>
+
+<style>
+.race {
+  background-image: url('../../public/congruent_pentagon.png');
+  height: 60vh;
+  width: 100%;
+}
+
+</style>
