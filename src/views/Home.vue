2f5a301e750cc94bfa9469e1b6467509d565a861
adityain98 <adityain98@gmail.com>
2019-10-18 10:19:03 +0700

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,17 +11,143 @@
     </div>
    </div>
     <img src="../../public/undraw_gaming_6oy3.svg" class="homeSvg">
+  <div class="container-fluid">
+    <div class="row d-flex flex-column justify-content-center align-items-center">
+      <div class="col-sm">
+        One of three columns
+      </div>
+      <div class="col-sm">
+        <div>
+          <b-button id="toggle-btn" @click="toggleModal">Toggle Modal</b-button>
+
+          <!-- Modal -->
+          <b-modal ref="my-modal" hide-footer title="Using Component Methods">
+            <b-form @submit.prevent="joinRoom()">
+              <div class="d-block">
+                <b-form-group
+                  id="input-name"
+                  label="Name:"
+                  label-for="name"
+                >
+                  <b-form-input
+                    id="name"
+                    type="text"
+                    required
+                    placeholder="Enter name"
+                    v-model="name"
+                  ></b-form-input>
+                </b-form-group>
+
+                <b-form-group
+                  id="input-room"
+                  label="Room Id:"
+                  label-for="room"
+                >
+                  <b-form-input
+                    id="room"
+                    type="text"
+                    required
+                    placeholder="Enter room id:"
+                    v-model="roomId"
+                  ></b-form-input>
+                </b-form-group>
+              </div>
+              <b-button class="mt-2" variant="outline-warning" block type="submit">Toggle Me</b-button>
+            </b-form>
+          </b-modal>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import firebase from '../configs/firebase'
+import db from '../configs/firestore'
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      name: '',
+      roomId: '',
+      createName: ''
+    }
+  },
+  methods: {
+    toggleModal() {
+      // We pass the ID of the button that we want to return focus to
+      // when the modal has hidden
+      this.$refs['my-modal'].toggle('#toggle-btn')
+    },
+    joinRoom(){
+      db.collection('rooms').get()
+        .then(doc=>{
+          let roomData = ''
+          let count = 0
+          let player1 = ''
+          let player2 = ''
+          let player3 = ''
+          let player4 = ''
+          doc.forEach(room=>{
+            if(room.id == this.roomId){
+              roomData = room.data()
+            }
+          })
+          if(roomData.count >4){
+            swal("Room is full", "Room is full join other room", "error");
+          }
+          else{
+            roomData.count += 1
+            let player = `player${roomData.count}`
+            roomData[player].name = this.name
+            // console.log(roomData[player]);
+            
+            count = roomData.count
+            player1 = roomData.player1
+            player2 = roomData.player2
+            player3 = roomData.player3
+            player4 = roomData.player4
+
+            db.collection('rooms').doc(this.roomId)
+            .set({
+              count,
+              player1,
+              player2,
+              player3,
+              player4
+            })
+          }
+        })
+    },
+    createRoom(){
+      db.collection("rooms").add({
+        count: 1,
+        player1: {
+          name: this.name,
+          pos: 0
+        },
+        player2: {
+          name: '',
+          pos: 0
+        },
+        player3: {
+          name: '',
+          pos: 0
+        },
+        player4: {
+          name: '',
+          pos: 0
+        },
+      })
+      .then(function(docRef) {
+        console.log(docRef.id);
+      })
+      .catch(function(error) {
+        console.error("Error adding document: ", error);
+      });
+
+    }
   }
 }
 </script>
